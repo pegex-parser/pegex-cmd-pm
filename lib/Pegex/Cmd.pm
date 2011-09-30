6318b78bb6650434433ea07e94807c3f186f8e3f
Ingy dot Net <ingy@ingy.net>
2011-09-30 13:10:12 +0200

    First commit

---
+++ lib/Pegex/Cmd.pm
@@ -0,0 +1,22 @@
+##
+# name:      Pegex::Cmd
+# abstract:  
+# author:    Ingy döt Net <ingy@ingy.net>
+# license:   perl
+# copyright: 2011
+
+use 5.010;
+use strict;
+use warnings;
+
+package Pegex::Cmd;
+
+1;
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
